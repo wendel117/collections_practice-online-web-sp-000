daf034ce8bb57cef06d9ec5e839003f39054d385
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-08 19:20:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
 
+def sort_array_asc(array)
+  array.sort!
+end
+
+
+def sort_array_desc(array)
+  array.sort.reverse
+
+end
+
+
+def sort_array_char_count
+
+end
+
+def swap_elements
+
+end
+
+def reverse_array(array)
+
+end
+
+def kesha_maker
+
+end
+
+def find_a
+
+end
+
+def sum_array
+
+end
+
+def add_s
+
+end
