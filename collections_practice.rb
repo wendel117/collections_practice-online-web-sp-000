4e1d39631ecde1155a6f4caddf1ec45ea92f3a02
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-08 19:47:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,9 +40,9 @@ end
 
 def add_s (array)
   array.collect.with_index do |word, index|
-    if index == 1 
-      word 
-    else 
+    if index == 1
+      word
+    else
       word + "s"
     end
   end
